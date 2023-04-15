5450bef607c43318d7f6953c82ff8c5c041f6a5c
James Hart <james.elliot.hart@gmail.com>
2023-04-15 16:52:04 -0500

    api path instead of root

--- feldspar/middle_tier/routes/index.js
+++ feldspar/middle_tier/routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/api", (req, res) => {
     res.send({ alive: true, at: (new Date()).toUTCString() }).status(200);
 });
 

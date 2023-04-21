6f73b92b1e8918350a92d3577df1462a81990416
James Hart <james.elliot.hart@gmail.com>
2023-04-21 16:55:41 -0500

    more clarity in health check route

--- feldspar/middle_tier/routes/index.js
+++ feldspar/middle_tier/routes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/api", (req, res) => {
-    res.send({ alive: true, at: (new Date()).toUTCString() }).status(200);
+router.get("/health", (req, res) => {
+    const response = { alive: true, at: (new Date()).toUTCString() };
+    console.log('Sending health status', response);
+    res.json(response);
 });
 
 module.exports = router;

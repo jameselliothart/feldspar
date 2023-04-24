2a695f4122dee03225e0b9febb470e617644d749
James Hart <james.elliot.hart@gmail.com>
2023-04-23 19:54:01 -0500

    implement caching

--- feldspar/MarketDataService/Program.cs
+++ feldspar/MarketDataService/Program.cs
@@ -6,15 +6,25 @@ var config = Configuration.FromEnvironmentVariables();
 
 var redis = ConnectionMultiplexer.Connect(config.RedisConnectionString);
 var sub = redis.GetSubscriber();
+var db = redis.GetDatabase();
 
 var alphaVantage = new AlphaVantage(config.AlphaVantageBaseUri, config.AlphaVantageApiKey);
 
 sub.Subscribe("MarketData.Query", async (ch, msg) => {
-    Console.WriteLine($"Received request for [{msg.ToString()}]");
-    var request = msg.ToString().Split('|');
-    var (commodity, interval) = (request[0], request[1]);
-    var data = await alphaVantage.GetCommodityData(commodity, interval);
-    sub.Publish("MarketData.Publish", data);
+    var requestKey = msg.ToString();
+    Console.WriteLine($"Received request for [{requestKey}] on channel [{ch.ToString()}]");
+    var cacheResult = await db.StringGetAsync(requestKey);
+    var result = cacheResult;
+    if (cacheResult.IsNullOrEmpty)
+    {
+        System.Console.WriteLine($"Cache miss for [{requestKey}]. Querying API.");
+        var requestItems = requestKey.Split('|');
+        var (commodity, interval) = (requestItems[0], requestItems[1]);
+        result = await alphaVantage.GetCommodityData(commodity, interval);
+        await db.StringSetAsync(requestKey, result, new TimeSpan(hours: 0, minutes: 5, seconds: 0), When.NotExists);
+    }
+    else {System.Console.WriteLine($"Cache hit for [{requestKey}].");}
+    sub.Publish("MarketData.Publish", result);
 });
 
 System.Console.WriteLine("MarketDataService has started.");

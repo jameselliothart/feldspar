c186c76edcf53a986183a49c678eb3c40be55991
James Hart <james.elliot.hart@gmail.com>
2023-04-28 06:44:36 -0500

    keep socket in state and (dis)connect with effect

--- feldspar/web/src/components/AssetVisualizer.js
+++ feldspar/web/src/components/AssetVisualizer.js
@@ -2,25 +2,38 @@ import React from 'react';
 import PriceChart from './PriceChart';
 import AssetSelector from './AssetSelector';
 import Commodities from '.././curve_config/commodities.json';
-import WTIData from '../.mockdata/WTI-Monthly.json'
 import openSocket from 'socket.io-client';
 
-const SOCKET = openSocket(process.env.REACT_APP_SOCKET_ENDPOINT);  // fails silently if not picked up
+// fails silently if not picked up
+// TODO this seems to create a lot of new connections - need to clean this up
 // TODO deal with 5 queries / minute
 // TODO probably limit to 4 curves at once
 export default function AssetVisualizer({ availableAssets = Commodities.functions }) {
-    const data = WTIData;
-    data.forEach(d => d.data.sort((a, b) => (new Date(a.date)) - (new Date(b.date))));
+    const [socket, setSocket] = React.useState(null);
     const [assetData, setAssetData] = React.useState([]);
 
+    React.useEffect(() => {
+        const ENDPOINT = process.env.REACT_APP_SOCKET_ENDPOINT;
+        console.log('Opening socket to', ENDPOINT)
+        const newSocket = openSocket(ENDPOINT);
+        console.log(`Visualizer socket connected: [${newSocket.id}]`);
+        setSocket(newSocket);
+
+
+        return () => {
+            console.log('Disconnecting', newSocket.id);
+            newSocket.disconnect();
+        }
+    }, [])
+
     const updateAssetSelection = (selectedAssets) => {
-        assetData.forEach(ad => SOCKET.off(`FromServer.Command.${ad.key}`));
+        assetData.forEach(ad => socket.off(`FromServer.Command.${ad.key}`));
         setAssetData([]);
         selectedAssets.forEach(asset => {
             const query = `${asset}|MONTHLY`;
-            SOCKET.on(`FromServer.Command.${query}`, d => setAssetData([...assetData, d]));
+            socket.on(`FromServer.Command.${query}`, d => setAssetData([...assetData, d]));
             console.log('FromClient.Query', query, 'emitted');
-            SOCKET.emit('FromClient.Query', query);
+            socket.emit('FromClient.Query', query);
         });
     };
 

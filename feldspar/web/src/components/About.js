e6a4b3fe269f45191c5fa12625f67a8b1d811cab
James Hart <james.elliot.hart@gmail.com>
2023-05-03 14:32:34 -0500

    update about

--- feldspar/web/src/components/About.js
+++ feldspar/web/src/components/About.js
@@ -1,10 +1,23 @@
+import { Link } from '@mui/material';
 import Typography from '@mui/material/Typography';
 
 export default function About() {
     return (
-        <Typography paragraph>
-            Feldspar is a market data visualization engine.
-            Click on the Asset Visualizer to get started!
-        </Typography>
+        <div>
+            <Typography paragraph>
+                Feldspar is a market data visualization engine.
+                Click on the Asset Visualizer to get started!
+            </Typography>
+            <Typography paragraph>
+                The App Service Plan is not very high-powered to keep the Azure bill low, so please don't go crazy trying to break the page.
+                This is a work in progress, so there are some bugs as well!
+            </Typography>
+            <Typography paragraph>
+                Take a look at the source code on
+                <Link href='https://github.com/jameselliothart/feldspar'>
+                    GitHub
+                </Link>
+            </Typography>
+        </div>
     )
 }

69cd21385e9262828852edce8dd965c0f8e2d4b5
James Hart <james.elliot.hart@gmail.com>
2023-05-10 17:16:09 -0500

    add java-based market data service

---
+++ feldspar/jMarketDataService/src/test/java/com/feldspar/MarketDataService/MarketDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feldspar.MarketDataService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
